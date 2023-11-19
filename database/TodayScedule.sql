1b411853a3ba48714bccb1e39b73f3f10c12754d
bilimited <1793678790@qq.com>
2023-11-19 21:58:28 +0800

    更新 TodayScedule.sql
    更新了数据库

--- database/TodayScedule.sql
+++ database/TodayScedule.sql
@@ -19,8 +19,8 @@
 CREATE DATABASE IF NOT EXISTS `ts` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ts`;
 
--- 导出  表 ts.class 结构
-CREATE TABLE IF NOT EXISTS `class` (
+-- 导出  表 ts.course 结构
+CREATE TABLE IF NOT EXISTS `course` (
   `cno` varchar(20) NOT NULL,
   `cname` varchar(20) DEFAULT NULL,
   `college` varchar(20) DEFAULT NULL,
@@ -33,38 +33,35 @@ CREATE TABLE IF NOT EXISTS `class` (
 CREATE TABLE IF NOT EXISTS `student` (
   `sno` varchar(20) NOT NULL DEFAULT '',
   `sname` varchar(20) DEFAULT '',
-  `college` varchar(20) DEFAULT '',
-  `major` varchar(20) DEFAULT '',
   `sex` varchar(10) DEFAULT '',
-  `class` varchar(20) DEFAULT '',
-  `phone` varchar(20) DEFAULT '',
   PRIMARY KEY (`sno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 数据导出被取消选择。
 
--- 导出  表 ts.student_class 结构
-CREATE TABLE IF NOT EXISTS `student_class` (
+-- 导出  表 ts.student_course 结构
+CREATE TABLE IF NOT EXISTS `student_course` (
   `sno` varchar(20) NOT NULL,
   `cno` varchar(20) NOT NULL,
   `semester` varchar(20) NOT NULL,
   `progress` int unsigned NOT NULL DEFAULT (0),
   PRIMARY KEY (`sno`,`cno`,`semester`),
   KEY `cno` (`cno`),
-  CONSTRAINT `cno` FOREIGN KEY (`cno`) REFERENCES `class` (`cno`),
+  CONSTRAINT `cno` FOREIGN KEY (`cno`) REFERENCES `course` (`cno`),
   CONSTRAINT `sno` FOREIGN KEY (`sno`) REFERENCES `student` (`sno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='student to class';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='student to course';
 
 -- 数据导出被取消选择。
 
 -- 导出  表 ts.teacher 结构
 CREATE TABLE IF NOT EXISTS `teacher` (
-  `tid` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `tno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `tname` varchar(20) DEFAULT NULL,
-  `college` varchar(20) DEFAULT NULL,
-  `major` varchar(20) DEFAULT NULL,
+  `cno` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `sex` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`tid`)
+  PRIMARY KEY (`tno`) USING BTREE,
+  KEY `fk_cno` (`cno`),
+  CONSTRAINT `fk_cno` FOREIGN KEY (`cno`) REFERENCES `course` (`cno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 数据导出被取消选择。
@@ -81,6 +78,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 数据导出被取消选择。
+
+-- 导出  表 ts.user_role 结构
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `uid` bigint NOT NULL DEFAULT '0',
+  `sno` varchar(50) DEFAULT NULL,
+  `tno` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `fk_sno` (`sno`),
+  KEY `fk_tno` (`tno`),
+  CONSTRAINT `fk_sno` FOREIGN KEY (`sno`) REFERENCES `student` (`sno`),
+  CONSTRAINT `fk_tno` FOREIGN KEY (`tno`) REFERENCES `teacher` (`tno`),
+  CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='User to Student or Teacher';
 
 -- 数据导出被取消选择。
 
